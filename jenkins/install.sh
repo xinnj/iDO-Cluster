0e6264c4f637b34b5b4fd75f9b7d72bfc7269370
xinnj <xinnj@hotmail.com>
2023-02-15 18:54:43 +0800

    jenkins

--- jenkins/install.sh
+++ jenkins/install.sh
@@ -1,3 +1,14 @@
 #! /bin/bash
+set -euaxo pipefail
 
+base=$(dirname "$0")
 
+# Create PVC
+# Todo: storageClassName, storage size
+kubectl apply -f "${base}"/pvc.yaml
+
+# Install Jenkins
+# Todo: set timezone
+# Todo: set url
+# Todo: set update center
+helm upgrade jenkins --install -f "${base}"/values-override.yaml "${base}"/jenkins-chart
