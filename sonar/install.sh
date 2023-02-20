77e4c2b6d51fd1e40d60b55123e0bc2cb0b1e837
xinnj <xinnj@hotmail.com>
2023-02-20 16:10:05 +0800

    Sonarqube

---
+++ sonar/install.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+set -euaxo pipefail
+
+base=$(dirname "$0")
+
+CONTAINER_MIRROR="${CONTAINER_MIRROR:-true}"
+
+# ceph-filesystem, nfs-client
+STORAGE_CLASS="ceph-filesystem"
+SONAR_STORAGE_SIZE="5Gi"
+SONAR_PG_STORAGE_SIZE="20Gi"
+
+# A trailing "/" must be included
+SONAR_URL_PREFIX="/sonarqube/"
+
+# Create PVC
+perl -0777 -p -i \
+    -e "s/<STORAGE_CLASS>/${STORAGE_CLASS}/g;" \
+    -e "s/<SONAR_STORAGE_SIZE>/${SONAR_STORAGE_SIZE}/g;" \
+    -e "s/<SONAR_PG_STORAGE_SIZE>/${SONAR_PG_STORAGE_SIZE}/g" \
+    "${base}"/pvc.yaml
+kubectl apply -f "${base}"/pvc.yaml
+
+# Install Sonarqube
+if [ "${CONTAINER_MIRROR}" == "true" ]; then
+    perl -0777 -p -i \
+        -e "s/docker\.io/docker.m.daocloud.io/g" \
+        "${base}"/values-override.yaml
+fi
+
+perl -0777 -p -i \
+    -e "s#<SONAR_URL_PREFIX>#${SONAR_URL_PREFIX}#g" \
+    "${base}"/values-override.yaml
+helm upgrade sonarqube --install -f "${base}"/values-override.yaml "${base}"/sonarqube-chart
