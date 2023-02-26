9b5368f15baa5539371c8c34c8a4c59376c4d9d1
xinnj <xinnj@hotmail.com>
2023-02-27 00:38:40 +0800

    Ceph supports single node mode

--- storage/ceph/install.sh
+++ storage/ceph/install.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
-set -euaxo pipefail
-
+set -euao pipefail
 base=$(dirname "$0")
 
 CONTAINER_MIRROR="${CONTAINER_MIRROR:-true}"
+
+node_num=$(kubectl get node --no-headers|wc -l)
+if (( node_num >= 3 )); then
+    install_mode="ha"
+else
+    install_mode="single"
+fi
+echo "Install ceph as $install_mode mode..."
 
 # Install rook-ceph operator
 if [ "${CONTAINER_MIRROR}" == "true" ]; then
@@ -11,9 +18,9 @@ if [ "${CONTAINER_MIRROR}" == "true" ]; then
         -e "s/docker\.io/docker.m.daocloud.io/g;" \
         -e "s/quay\.io/quay.m.daocloud.io/g;" \
         -e "s/registry\.k8s\.io/k8s.m.daocloud.io/g" \
-        "${base}"/values-rook-ceph.yaml
+        "${base}/values-rook-ceph-${install_mode}.yaml"
 fi
-helm upgrade rook-ceph --install --create-namespace --namespace rook-ceph --wait --wait-for-jobs --timeout 10m -f "${base}"/values-rook-ceph.yaml "${base}"/rook-ceph-chart
+helm upgrade rook-ceph --install --create-namespace --namespace rook-ceph --wait --wait-for-jobs --timeout 10m -f "${base}/values-rook-ceph-${install_mode}.yaml" "${base}/rook-ceph-chart"
 
 # Install ceph cluster
 if [ "${CONTAINER_MIRROR}" == "true" ]; then
@@ -21,7 +28,6 @@ if [ "${CONTAINER_MIRROR}" == "true" ]; then
         -e "s/docker\.io/docker.m.daocloud.io/g;" \
         -e "s/quay\.io/quay.m.daocloud.io/g;" \
         -e "s/registry\.k8s\.io/k8s.m.daocloud.io/g" \
-        "${base}"/values-rook-ceph-cluster.yaml
+        "${base}/values-rook-ceph-cluster-${install_mode}.yaml"
 fi
-helm upgrade rook-ceph-cluster --install --create-namespace --namespace rook-ceph -f "${base}"/values-rook-ceph-cluster.yaml "${base}"/rook-ceph-cluster-chart
-kubectl --namespace rook-ceph get cephcluster
+helm upgrade rook-ceph-cluster --install --create-namespace --namespace rook-ceph -f "${base}/values-rook-ceph-cluster-${install_mode}.yaml" "${base}/rook-ceph-cluster-chart"
