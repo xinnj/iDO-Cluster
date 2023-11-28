6543a399ae2254de2de3e5a24bcef92a34cef6b9
xinnj <xinnj@hotmail.com>
2023-11-28 14:50:38 +0800

    Cert-manager

---
+++ packages/cert-manager/install.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+set -euao pipefail
+
+base=$(dirname "$0")
+
+echo "##########################################################################"
+echo "### Install Cert-manager ###"
+
+envsubst < "${base}/values-override.yaml" > "${base}/values.yaml"
+helm upgrade cert-manager --install --create-namespace --namespace cert-manager --wait --timeout 30m -f "${base}"/values.yaml "${base}"/cert-manager-chart
+
+envsubst < "${base}/cluster-issuer-template.yaml" > "${base}/cluster-issuer.yaml"
+kubectl apply -f "${base}/cluster-issuer.yaml"
