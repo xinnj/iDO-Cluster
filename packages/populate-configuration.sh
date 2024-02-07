44e53d00a97a6e4c7343930319ce4778f0698edb
xinnj <xinnj@hotmail.com>
2024-02-07 17:10:30 +0800

    Revert "Fix packages-env cm"
    This reverts commit 38e3e1ff19f6e39bb8e87123f96a45daf06ff486.

--- packages/populate-configuration.sh
+++ packages/populate-configuration.sh
@@ -6,7 +6,7 @@ echo "### Populate Configuration ###"
 
 export -p | awk '{print $3'} | grep "^IDO" | tr -d '"' > packages_env_new.txt
 
-kubectl get cm packages-env --ignore-not-found=true -o jsonpath="{.data.packages_env\.txt}" > packages_env_exist.txt
+kubectl get cm packages-env --ignore-not-found=true -o jsonpath="{.data}" | jq -r 'to_entries[] | .key + "=" + .value' > packages_env_exist.txt
 declare -A packages_env_exist
 while IFS='=' read -r k v; do
   packages_env_exist[$k]=$v
@@ -21,10 +21,10 @@ for k in "${!packages_env_new[@]}"; do
   packages_env_exist[$k]="${packages_env_new[$k]}"
 done
 
-rm -f packages_env.txt
+rm -f packages_env_all.txt
 for k in "${!packages_env_exist[@]}"; do
-  echo "$k=${packages_env_exist[$k]}" >> packages_env.txt
+  echo "$k=${packages_env_exist[$k]}" >> packages_env_all.txt
 done
 
 kubectl delete -n ${IDO_TEAM} cm packages-env --ignore-not-found=true
-kubectl create -n ${IDO_TEAM} cm packages-env --from-file=./packages_env.txt
+kubectl create -n ${IDO_TEAM} cm packages-env --from-env-file=./packages_env_all.txt
