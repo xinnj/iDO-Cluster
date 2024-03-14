7c60673b2323846ebaaa8e9bfe6ab732931f9fa4
xinnj <xinnj@hotmail.com>
2024-03-14 21:29:00 +0800

    Fix to show pods of all ns

--- packages/final-check.sh
+++ packages/final-check.sh
@@ -3,7 +3,7 @@ set -euao pipefail
 
 not_ready_pod_list() {
   pod_list=""
-  for p in $(kubectl -n ${IDO_TEAM} get pods --field-selector=status.phase!=Running,status.phase!=Succeeded -o=jsonpath="{range .items[*]}{.metadata.name}{';'}{.metadata.ownerReferences[?(@.kind != 'Job')].name}{'\n'}{end}"); do
+  for p in $(kubectl get pods -A --field-selector=status.phase!=Running,status.phase!=Succeeded -o=jsonpath="{range .items[*]}{.metadata.name}{';'}{.metadata.ownerReferences[?(@.kind != 'Job')].name}{'\n'}{end}"); do
     v_owner_name=$(echo $p | cut -d';' -f2)
     if [ ! -z "$v_owner_name" ]; then
       v_pod_name=$(echo $p | cut -d';' -f1)
@@ -20,7 +20,7 @@ echo "##########################################################################
 result=$(not_ready_pod_list)
 if [ "${result}" != "" ]; then
   echo "Please wait for these pods to be ready..."
-  kubectl get pods $result
+  kubectl get pods -A --field-selector=status.phase!=Running,status.phase!=Succeeded -o=custom-columns='NAMESPACE:.metadata.namespace,NAME:.metadata.name,READY:.status.containerStatuses[*].ready,STATUS:.status.phase,RESTARTS:.status.containerStatuses[*].restartCount,OWNER-KIND:.metadata.ownerReferences[*].kind'|grep -v 'Job'|grep -v 'Challenge'
 fi
 
 while [ "$result" != "" ]; do
@@ -31,7 +31,7 @@ while [ "$result" != "" ]; do
     if [ "${new_result}" != "${result}" ]; then
       echo "##########################################################################"
       echo "Please wait for these pods to be ready..."
-      kubectl get pods $new_result
+      kubectl get pods -A --field-selector=status.phase!=Running,status.phase!=Succeeded -o=custom-columns='NAMESPACE:.metadata.namespace,NAME:.metadata.name,READY:.status.containerStatuses[*].ready,STATUS:.status.phase,RESTARTS:.status.containerStatuses[*].restartCount,OWNER-KIND:.metadata.ownerReferences[*].kind'|grep -v 'Job'|grep -v 'Challenge'
     fi
   fi
   result=${new_result}
