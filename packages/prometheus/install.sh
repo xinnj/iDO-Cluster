6c6fa8421cee36c6a24f66865c1747f886571555
xinnj <xinnj@hotmail.com>
2024-07-02 19:52:56 +0800

    Prometheus: alertmanager config; dingtalk webhook

--- packages/prometheus/install.sh
+++ packages/prometheus/install.sh
@@ -10,3 +10,8 @@ echo "### Install Prometheus Stack ###"
 envsubst < "${base}/values-override.yaml" > "${base}/values.yaml"
 "${base}/../check-undefined-env.sh" "${base}/values.yaml"
 helm upgrade prometheus --install --create-namespace --namespace monitoring --timeout 30m -f "${base}"/values.yaml "${base}"/kube-prometheus-stack
+
+# Install dingtalk webhook
+envsubst < "${base}/values-override-dingtalk.yaml" > "${base}/values-dingtalk.yaml"
+"${base}/../check-undefined-env.sh" "${base}/values-dingtalk.yaml"
+helm upgrade prometheus-webhook-dingtalk --install --create-namespace --namespace monitoring --timeout 30m -f "${base}"/values-dingtalk.yaml "${base}"/prometheus-webhook-dingtalk
---
+++ packages/prometheus/prometheus-webhook-dingtalk/README.md
@@ -0,0 +1,2 @@
+# helm-chart-starter-deployment
+A helm chart starter including ingress, service, deployment and pvc
